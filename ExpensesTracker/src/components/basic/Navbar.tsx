7aa31998ff43827df22ba35c259341f59637c8c3
Naral <77269201+NaralC@users.noreply.github.com>
2023-05-22 19:23:10 +0100

    Responsive navbar with drawer
    - Placeholder pages for the route

---
+++ ExpensesTracker/src/components/basic/Navbar.tsx
@@ -0,0 +1,153 @@
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import CssBaseline from "@mui/material/CssBaseline";
+import Divider from "@mui/material/Divider";
+import Drawer from "@mui/material/Drawer";
+import IconButton from "@mui/material/IconButton";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemText from "@mui/material/ListItemText";
+import MenuIcon from "@mui/icons-material/Menu";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import { Link } from "react-router-dom";
+import { FC, useState } from "react";
+import routes from "../../constants/routes";
+
+interface Props {
+  /**
+   * Injected by the documentation to work in an iframe.
+   * You won't need it on your project.
+   */
+  window?: () => Window;
+}
+
+const drawerWidth = 240;
+
+const Navbar: FC<Props> = ({ window }) => {
+  const [mobileOpen, setMobileOpen] = useState(false);
+
+  const handleDrawerToggle = () => {
+    setMobileOpen((prevState) => !prevState);
+  };
+
+  const drawer = (
+    <Box onClick={handleDrawerToggle} sx={{ textAlign: "center" }}>
+      <Typography variant="h6" sx={{ my: 2 }}>
+        MUI
+      </Typography>
+      <Divider />
+      <List
+        sx={{
+          display: "flex",
+          flexDirection: "column",
+          alignContent: "middle",
+          alignItems: "middle",
+        }}
+      >
+        {routes.map((item) => (
+          <ListItem key={item} disablePadding>
+            <ListItemButton sx={{ textAlign: "center" }}>
+              <Link
+                to={`/${item}`}
+                style={{
+                  textDecoration: "none",
+                  color: "black",
+                  width: "100%",
+                }}
+              >
+                <ListItemText primary={item} />
+              </Link>
+            </ListItemButton>
+          </ListItem>
+        ))}
+      </List>
+    </Box>
+  );
+
+  const container =
+    window !== undefined ? () => window().document.body : undefined;
+
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+      <AppBar component="nav">
+        <Toolbar>
+          <IconButton
+            color="inherit"
+            aria-label="open drawer"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2, display: { sm: "none" } }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Box sx={{ display: { xs: "none", sm: "block" }, flexGrow: 1 }}>
+            {routes.map((item) => (
+              <Button key={item} sx={{ color: "#fff", mx: 2 }}>
+                <Link
+                  to={`/${item}`}
+                  style={{ textDecoration: "none", color: "white" }}
+                >
+                  <Typography
+                    variant="h5"
+                    sx={{
+                      textTransform: "none",
+                      fontSize: {
+                        sm: "1.0rem",
+                        md: "1.3rem",
+                      },
+                    }}
+                  >
+                    {item}
+                  </Typography>
+                </Link>
+              </Button>
+            ))}
+          </Box>
+          <Typography
+            variant="h6"
+            component="div"
+            sx={{
+              display: { xs: "none", sm: "block" },
+              fontSize: {
+                sm: "1.0rem",
+                md: "1.3rem",
+              },
+            }}
+          >
+            Login
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <Box component="nav">
+        <Drawer
+          container={container}
+          variant="temporary"
+          open={mobileOpen}
+          onClose={handleDrawerToggle}
+          ModalProps={{
+            keepMounted: true, // Better open performance on mobile.
+          }}
+          sx={{
+            display: { xs: "block", sm: "none" },
+            "& .MuiDrawer-paper": {
+              boxSizing: "border-box",
+              width: drawerWidth,
+            },
+          }}
+        >
+          {drawer}
+        </Drawer>
+      </Box>
+      <Box component="main" sx={{ p: 3 }}>
+        <Toolbar />
+        <Typography>Page Content</Typography>
+      </Box>
+    </Box>
+  );
+};
+
+export default Navbar;
---
+++ ExpensesTracker/src/pages/NetworthPage.tsx
@@ -0,0 +1,7 @@
+import { FC } from "react";
+
+const NetworthPage: FC = () => {
+    return <div>Networth Page</div>
+}
+
+export default NetworthPage;
