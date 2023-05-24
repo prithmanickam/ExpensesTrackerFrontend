4d93d7004de0ae556319a4f7897e17acfecf6aac
Naral <77269201+NaralC@users.noreply.github.com>
2023-05-24 22:50:33 +0100

    Skeleton frontend
    Co-authored-by: Prith Manickam (student) <2518533M@student.gla.ac.uk>

--- ExpensesTracker/src/pages/HomePage.tsx
+++ ExpensesTracker/src/pages/HomePage.tsx
@@ -1,102 +1,121 @@
 import { Stack } from "@mui/material";
-import { FC } from "react";
+import { FC, useContext, useEffect, useRef, useState } from "react";
 import { Chart } from "react-google-charts";
+import {
+  data as chartsData,
+  options,
+  stackedBarData,
+  stackedBarOptions,
+  barChartData,
+  barChartOptions,
+} from "../constants/chartsMockData";
 
-export const data = [
-  ["Task", "Hours per Day"],
-  ["Work", 11],
-  ["Eat", 2],
-  ["Commute", 2],
-  ["Watch TV", 2],
-  ["Sleep", 7],
-];
-
-export const options = {
-    title: "My Daily Activities",
-    pieHole: 0.4,
-    is3D: false,
-  };
-
-  export const barChartData = [
-    ["City", "2010 Population", "2000 Population"],
-    ["New York City, NY", 8175000, 8008000],
-    ["Los Angeles, CA", 3792000, 3694000],
-    ["Chicago, IL", 2695000, 2896000],
-    ["Houston, TX", 2099000, 1953000],
-    ["Philadelphia, PA", 1526000, 1517000],
-  ];
-  
-  export const barChartOptions = {
-    title: "Population of Largest U.S. Cities",
-    chartArea: { width: "50%" },
-    colors: ["#b0120a", "#ffab91"],
-    hAxis: {
-        title: "Total Population",
-    minValue: 0,
-  },
-  vAxis: {
-    title: "City",
-  },
+type Transaction = {
+  expenseName: string;
+  expenseCategory:
+    | "ENTERTAINMENT"
+    | "GROCERIES"
+    | "RESTAURANT"
+    | "UTILITIES"
+    | "MISC";
+  expenseAmount: number;
+  expenseDate: string;
 };
 
-  export const stackedBarData = [
-    ["City", "2010 Population", "2000 Population"],
-    ["New York City, NY", 8175000, 8008000],
-    ["Los Angeles, CA", 3792000, 3694000],
-    ["Chicago, IL", 2695000, 2896000],
-    ["Houston, TX", 2099000, 1953000],
-    ["Philadelphia, PA", 1526000, 1517000],
+const getMockedData = (): Transaction[] => {
+  return [
+    {
+      expenseName: "Fish",
+      expenseCategory: "GROCERIES",
+      expenseAmount: 10,
+      expenseDate: "10-10-2023",
+    },
+    {
+      expenseName: "Tesco Food",
+      expenseCategory: "GROCERIES",
+      expenseAmount: 5,
+      expenseDate: "03-05-2017",
+    },
   ];
-  
-  export const stackedBarOptions = {
-    title: "Population of Largest U.S. Cities",
-    chartArea: { width: "50%" },
-    isStacked: true,
-    hAxis: {
-      title: "Total Population",
-      minValue: 0,
-    },
-    vAxis: {
-      title: "City",
-    },
-  };
+};
 
+// @Field(name = "name")
+//     @Indexed(unique = true)
+//     private String expenseName;
+
+//     @Field(name = "category")
+//     private ExpenseCategory expenseCategory;
+
+//     @Field(name = "amount")
+//     private BigDecimal expenseAmount;
+
+// public enum ExpenseCategory {
+//   ENTERTAINMENT, GROCERIES, RESTAURANT, UTILITIES, MISC
+// }
 
 const HomePage: FC = () => {
-    return (
+  const [data, setData] = useState<Transaction[]>(getMockedData());
+  // const dataRef = useRef(data);
+
+  useEffect(() => {
+    // Executes when dependency array changes
+    // pushToHomePage();
+  }, [data]);
+
+  useEffect(() => {
+    // Executes on page load
+  }, []);
+
+  // useContext();
+
+  return (
+    <div>
+      <Stack
+        direction={{ xs: "column", sm: "row" }}
+        justifyContent="center"
+        alignItems="center"
+        spacing={2}
+      >
+        <Chart
+          chartType="PieChart"
+          data={chartsData}
+          options={options}
+          width={"100%"}
+          height={"400px"}
+        />
+
+        <Chart
+          chartType="BarChart"
+          width="100%"
+          height="400px"
+          data={stackedBarData}
+          options={stackedBarOptions}
+        />
+
+        <Chart
+          chartType="BarChart"
+          width="100%"
+          height="400px"
+          data={barChartData}
+          options={barChartOptions}
+        />
+      </Stack>
+
       <div>
-    <Stack
-     direction={{ xs: 'column', sm: 'row' }}
-  justifyContent="center"
-  alignItems="center"
-  spacing={2}>
-    <Chart
-      chartType="PieChart"
-      data={data}
-      options={options}
-      width={"100%"}
-      height={"400px"}
-    />
+        {data.map((d) => {
+          const { expenseAmount, expenseCategory, expenseDate, expenseName  } = d
 
-    <Chart
-      chartType="BarChart"
-      width="100%"
-      height="400px"
-      data={stackedBarData}
-      options={stackedBarOptions}
-    />
-
-    <Chart
-      chartType="BarChart"
-      width="100%"
-      height="400px"
-      data={barChartData}
-      options={barChartOptions}
-    />
-    </Stack>
-    
-        
+          return (
+            <div>
+              <div>{expenseAmount}</div>
+              <div>{expenseCategory}</div>
+              <div>{expenseDate}</div>
+              <div>{expenseName}</div>
+            </div>
+          )
+        })}
+      </div>
     </div>
-    );
-}
+  );
+};
 export default HomePage;
