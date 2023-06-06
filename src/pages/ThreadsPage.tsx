1fe73f7ce115f021eb775a29d50581e5c869f57f
Naral <77269201+NaralC@users.noreply.github.com>
2023-06-06 22:41:48 +0700

    Barebones post/del data on backend

--- src/pages/ThreadsPage.tsx
+++ src/pages/ThreadsPage.tsx
@@ -1,8 +1,10 @@
 import { Button, Stack, TextField, Typography } from "@mui/material";
-import { FC, useContext, useState } from "react";
+import { FC, useContext, useEffect, useRef, useState } from "react";
 
 import CommentCard from "../components/CommentCard";
-import { CommentContext } from "../context/CommentContext";
+import { Comment, CommentContext } from "../context/CommentContext";
+import axios from "axios";
+import toast from "react-hot-toast";
 
 const ThreadsPage: FC = () => {
   const { comments, addComment } = useContext(CommentContext);
@@ -29,6 +31,51 @@ const ThreadsPage: FC = () => {
     });
   };
 
+  // Fetches all threads
+  const effectRan = useRef(false);
+  useEffect(() => {
+    const fetchAllPosts = async () => {
+      try {
+        const response = await axios({
+          method: "get",
+          url: "http://localhost:8080/api/post",
+          headers: {
+            "Content-Type": "application/json",
+          },
+        });
+
+        const data = response.data;
+
+        console.log(data);
+        toast.success("Fetched all posts");
+      } catch (error) {
+        toast.error("Could not posts");
+      }
+    };
+
+    if (effectRan.current === false) fetchAllPosts();
+
+    return () => {
+      effectRan.current = true;
+    };
+  }, []);
+
+  const handleCreateNewPost = async () => {
+    try {
+      const response = await axios.post("http://localhost:8080/api/post", {
+        poster: "JAKEJAKEJAKEJAKEJAKEJAKEJAKEJAKEJAKE",
+        text: "yo this is my comment!",
+        likes: 15,
+        replies: ["looks dope!", "jk", "ratio"],
+      });
+
+      console.log(response);
+      toast.success("Post sent to backend!");
+    } catch (error) {
+      toast.error((error as Error).message);
+    }
+  };
+
   return (
     <Stack
       display="flex"
@@ -46,6 +93,9 @@ const ThreadsPage: FC = () => {
       />
       <Button variant="contained" onClick={handleCommentSubmit}>
         Submit
+      </Button>
+      <Button variant="contained" onClick={handleCreateNewPost}>
+        Create new post (with fixed data)
       </Button>
 
       {comments.map(
