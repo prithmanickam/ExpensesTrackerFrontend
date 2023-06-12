1f7cd67f1190ce79e5f9ba992ae7ce448845b8a7
Naral <77269201+NaralC@users.noreply.github.com>
2023-06-13 00:02:00 +0700

    GET request for fetching all requests from mongo

--- next-migration/app/api/db/route.ts
+++ next-migration/app/api/db/route.ts
@@ -1,19 +1,42 @@
-import { NextResponse } from 'next/server'
- 
+// @ts-ignore
+import clientPromise from "@/lib/actions/mongoDb";
+import { NextResponse } from "next/server";
+
 export async function GET(request: Request) {
-    const res = await request.json()
- 
-  return NextResponse.json({ data: 'This is made up' })
+  // const req = await request.json();
+  // console.log(req);
+
+  try {
+    // @ts-ignore
+    const client = await clientPromise;
+    const db = client.db("expense-tracker");
+
+    const posts = await db
+      .collection("post")
+      .find({})
+      .sort({ metacritic: -1 })
+      .limit(10)
+      .toArray();
+
+      console.log(posts)
+
+    return NextResponse.json({ data: posts });
+  } catch (error) {
+    console.log((error as Error).message);
+    return new NextResponse('bro what', {
+      status: 404
+    })
+  }
 }
 
 export async function POST(request: Request) {
-    const res = await request.json()
- 
-  return NextResponse.json({ data: 'This is made up' })
+  const res = await request.json();
+
+  return NextResponse.json({ data: "This is made up" });
 }
 
 export async function DELETE(request: Request) {
-    const res = await request.json()
- 
-  return NextResponse.json({ data: 'This is made up' })
+  const res = await request.json();
+
+  return NextResponse.json({ data: "This is made up" });
 }
