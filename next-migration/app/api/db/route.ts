791022fc9dff5732a7b8100fbedbc596d6c021dd
Naral <77269201+NaralC@users.noreply.github.com>
2023-06-13 14:39:25 +0700

    Post and Delete handlers + schema for posts

--- next-migration/app/api/db/route.ts
+++ next-migration/app/api/db/route.ts
@@ -1,11 +1,9 @@
 // @ts-ignore
 import clientPromise from "@/lib/actions/mongoDb";
-import { NextResponse } from "next/server";
+import { DeleteOnePostSchema, PostArraySchema, PostSchema } from "@/lib/validator/post";
+import { NextRequest, NextResponse } from "next/server";
 
 export async function GET(request: Request) {
-  // const req = await request.json();
-  // console.log(req);
-
   try {
     // @ts-ignore
     const client = await clientPromise;
@@ -18,25 +16,69 @@ export async function GET(request: Request) {
       .limit(10)
       .toArray();
 
-      console.log(posts)
+    console.log(posts);
 
-    return NextResponse.json({ data: posts });
+    return NextResponse.json(
+      { data: posts },
+      {
+        status: 200,
+      }
+    );
   } catch (error) {
     console.log((error as Error).message);
-    return new NextResponse('bro what', {
-      status: 404
-    })
+    return new NextResponse("Get Error", {
+      status: 404,
+    });
   }
 }
 
 export async function POST(request: Request) {
-  const res = await request.json();
+  const obj = await request.json();
+  try {
+    const parsedPost = PostSchema.parse(obj);
+    
+    // @ts-ignore
+    const client = await clientPromise;
+    const db = client.db("expense-tracker");
 
-  return NextResponse.json({ data: "This is made up" });
+    const posts = await db.collection("post").insertOne(parsedPost);
+
+    return new NextResponse("Post created", {
+      status: 201,
+    });
+  } catch (error) {
+    console.log((error as Error).message);
+
+    let badRequest = false;
+    if ((error as any).issues[0].code === "invalid_type") badRequest = true;
+
+    return new NextResponse("Post Error", {
+      status: badRequest ? 400 : 404,
+    });
+  }
 }
 
 export async function DELETE(request: Request) {
-  const res = await request.json();
+  // TODO: Make delete one item work. Right now only delete-all works
+  // const obj = JSON.parse(JSON.stringify(request));
+  // console.log(obj)
+  // const parsedId = DeleteOnePostSchema.parse(obj)
+  try {
+    // console.log(id)
 
-  return NextResponse.json({ data: "This is made up" });
+    // @ts-ignore
+    const client = await clientPromise;
+    const db = client.db("expense-tracker");
+
+    const posts = await db.collection("post").deleteMany();
+    // const posts = await db.collection("post").deleteOne(id);
+    
+    return new NextResponse("All Posts deleted", {
+      status: 200,
+    });
+  } catch (error) {
+    return new NextResponse("Delete Error", {
+      status: 404,
+    });
+  }
 }
---
+++ next-migration/lib/validator/post.ts
@@ -0,0 +1,18 @@
+import { z } from 'zod';
+
+export const PostSchema = z.object({
+    id: z.string(),
+    text: z.string(),
+    likes: z.string(),
+    date: z.date(),
+    poster: z.string(),
+    replies: z.array(z.string())
+})
+
+export const PostArraySchema = z.array(PostSchema);
+
+export type Post = z.infer<typeof PostSchema>;
+
+export const DeleteOnePostSchema = z.object({
+    id: z.string()
+})
